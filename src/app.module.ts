e9631ce94c5b3f44f5620ff6e8de2d2ffd896f2b
s14228so <saongtx7@gmail.com>
2020-01-24 02:41:40 +0900

    Init

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'nestday7',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  providers: [UsersService],
+})
+export class AppModule { }
