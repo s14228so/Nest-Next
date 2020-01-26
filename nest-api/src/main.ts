5bd67e3ce29328ba15b861cc6bd1f00bd7802c98
s14228so <saongtx7@gmail.com>
2020-01-26 23:44:23 +0900

    corsとprefix v1 added

--- nest-api/src/main.ts
+++ nest-api/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("v1")
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
   await app.listen(3000);
 }
 bootstrap();
