15c0a133ea45d8b159eebb5a81c29e160a23b150
s14228so <saongtx7@gmail.com>
2020-01-29 12:51:49 +0900

    logout button 配置

--- front/components/layouts/Header.tsx
+++ front/components/layouts/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import { AppBar, Toolbar, Typography, IconButton, Button } from '@material-ui/core';
+
 import Link from "next/link"
 import { grey } from '@material-ui/core/colors';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { useSelector } from 'react-redux';
+import { State } from '../../store';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -18,11 +19,18 @@ const useStyles = makeStyles((theme: Theme) =>
         title: {
             flexGrow: 1,
         },
+        home: {
+            display: "block",
+            marginRight: 50
+        }
     }),
 );
 
+const loginUser = (state: State) => state.user
+
 export default function ButtonAppBar() {
     const classes = useStyles({});
+    const user = useSelector(loginUser)
     const theme = createMuiTheme({
         palette: {
             primary: {
@@ -37,10 +45,29 @@ export default function ButtonAppBar() {
                 <AppBar position="static" color="primary">
                     <Toolbar>
                         <Typography variant="h6" className={classes.title}>
-                            <Link href="/">
+                            <Link href="/" >
                                 <a style={{ color: "#000" }}> Home</a>
                             </Link>
+
                         </Typography>
+                        {!user && (
+                            <div>
+                                <IconButton aria-label="display more actions" edge="end" color="inherit">
+                                    <Link href="/login">
+                                        <a style={{ color: "#000" }}> Sign Up</a>
+                                    </Link>
+                                </IconButton>
+                                <IconButton aria-label="display more actions" edge="end" color="inherit">
+                                    <Link href="/signup">
+                                        <a style={{ color: "#000" }}> Login</a>
+                                    </Link>
+                                </IconButton>
+                            </div>
+                        )}
+                        {user &&
+                            <IconButton aria-label="display more actions" edge="end" color="inherit">
+                                <Button>LogOut</Button>
+                            </IconButton>}
                     </Toolbar>
                 </AppBar>
             </ThemeProvider>
